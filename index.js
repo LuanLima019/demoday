5676b5735388b2157c4b659e0ecdf98f1b9b0cec
luanlima019 <luanlima019@gmail.com>
2018-08-31 11:21:14 -0300

    Adicionar funcionalidade ao faleconosco para as msgs salvar no banco de dados

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const eventos = require('./data/eventos.json');
+const bodyParser = require('body-parser');
+const expressMongoDb = require('express-mongo-db'); 
 const app = express();
 
 
 
 app.set('view engine', 'ejs');
 app.use('/static', express.static('static'));
+app.use(bodyParser.urlencoded());
+app.use(expressMongoDb('mongodb://localhost/Cultural'));
+
 app.get('', (req, res) => {
     res.render('index');
+});
+
+app.post('', (req, res) => {
+    req.db.collection('mensagens').insert(req.body, (erro) =>{
+        console.log(erro);
+        res.render('Obrigado');
+    });
 });
 
 app.get('/cultural', (req, res) => {
